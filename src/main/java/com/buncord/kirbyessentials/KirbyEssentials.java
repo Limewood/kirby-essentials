f45750bb36695d3373289f9aa7117140bafb6a9c
Joakim Lindskog <epost@joakimlindskog.se>
2023-09-27 22:11:45 +0200

    Add Potion of Flight

--- src/main/java/com/buncord/kirbyessentials/KirbyEssentials.java
+++ src/main/java/com/buncord/kirbyessentials/KirbyEssentials.java
@@ -2,9 +2,11 @@ package com.buncord.kirbyessentials;
 
 import com.buncord.kirbyessentials.block_entities.ModBlockEntities;
 import com.buncord.kirbyessentials.blocks.ModBlocks;
+import com.buncord.kirbyessentials.effect.ModEffects;
 import com.buncord.kirbyessentials.entities.ModEntities;
 import com.buncord.kirbyessentials.items.ModItems;
 import com.buncord.kirbyessentials.paintings.ModPaintings;
+import com.buncord.kirbyessentials.potion.ModPotions;
 import com.buncord.kirbyessentials.sounds.ModSounds;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -26,5 +28,7 @@ public class KirbyEssentials {
 		ModItems.register(eventBus);
 		ModSounds.register(eventBus);
 		ModPaintings.register(eventBus);
+		ModEffects.register(eventBus);
+		ModPotions.register(eventBus);
 	}
 }
