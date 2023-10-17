ab550192133a5cedec3e1c63addde71ba4fd915b
Limewood <epost@joakimlindskog.se>
2023-10-17 19:08:25 +0200

    Add fanart from mikekilluz (#30)
    * Add fanart from mikekilluz
    
    * Make kort_peep 2x1
    
    * Add background to briony_nyehehe

--- src/main/java/com/buncord/kirbyessentials/paintings/ModPaintings.java
+++ src/main/java/com/buncord/kirbyessentials/paintings/ModPaintings.java
@@ -31,6 +31,9 @@ public class ModPaintings {
     public static final RegistryObject<Motive> NO_THOUGHTS = registerPainting("no_thoughts",64,48);
     public static final RegistryObject<Motive> NOT_SHITTING = registerPainting("not_shitting",64,48);
     public static final RegistryObject<Motive> STINKY_OUT = registerPainting("stinky_out",64,48);
+    public static final RegistryObject<Motive> BRIONY_NYEHEHE = registerPainting("briony_nyehehe",16,16);
+    public static final RegistryObject<Motive> KORT_PEEP = registerPainting("kort_peep",32,16);
+    public static final RegistryObject<Motive> THE_SILLIES = registerPainting("the_sillies",64,48);
 
     public static final RegistryObject<Motive> BABY2 = registerPainting("baby2",64,48);
 
