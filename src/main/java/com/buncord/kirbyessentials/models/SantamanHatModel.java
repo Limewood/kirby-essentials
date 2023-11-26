8f5591ca9a6c03d3e0ba7c65512d8d7e9a04ffb9
Joakim Lindskog <epost@joakimlindskog.se>
2023-11-26 18:49:19 +0100

    Make santa hat model
    Update version

--- src/main/java/com/buncord/kirbyessentials/models/SantamanHatModel.java
+++ src/main/java/com/buncord/kirbyessentials/models/SantamanHatModel.java
@@ -2,6 +2,7 @@ package com.buncord.kirbyessentials.models;
 
 import com.buncord.kirbyessentials.KirbyEssentials;
 import com.google.common.collect.ImmutableList;
+import com.mojang.math.Vector3f;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
@@ -16,6 +17,9 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @OnlyIn(Dist.CLIENT)
 public class SantamanHatModel<T extends LivingEntity> extends CosmeticArmorModel<T> {
@@ -32,63 +36,99 @@ public class SantamanHatModel<T extends LivingEntity> extends CosmeticArmorModel
       "textures/models/armor/santaman_hat.png"
   );
 
-  public final ModelPart hat;
-  public final ModelPart leftEar;
-  public final ModelPart rightEar;
+  public final ModelPart rim;
+  public final ModelPart layer1;
+  public final ModelPart layer2;
+  public final ModelPart layer3;
+  public final ModelPart bobble;
+  private final Map<ModelPart, Vector3f> partPos;
+  private final Map<ModelPart, Vector3f> partRot;
+
+  private static final String RIM = "rim";
+  private static final String LAYER1 = "layer1";
+  private static final String LAYER2 = "layer2";
+  private static final String LAYER3 = "layer3";
+  private static final String BOBBLE = "bobble";
 
   public SantamanHatModel(ModelPart modelPart) {
     super(modelPart);
 
-    this.hat = modelPart.getChild("hat");
-    this.leftEar = modelPart.getChild("left_ear");
-    this.rightEar = modelPart.getChild("right_ear");
+    this.rim = modelPart.getChild(RIM);
+    this.layer1 = modelPart.getChild(LAYER1);
+    this.layer2 = modelPart.getChild(LAYER2);
+    this.layer3 = modelPart.getChild(LAYER3);
+    this.bobble = modelPart.getChild(BOBBLE);
+    this.partPos = new HashMap<>();
+    this.partRot = new HashMap<>();
+    addPosAndRot(this.rim);
+    addPosAndRot(this.layer1);
+    addPosAndRot(this.layer2);
+    addPosAndRot(this.layer3);
+    addPosAndRot(this.bobble);
+  }
+
+  private void addPosAndRot(ModelPart part) {
+    this.partPos.put(part, new Vector3f(part.x, part.y, part.z));
+    this.partRot.put(part, new Vector3f(part.xRot, part.yRot, part.zRot));
   }
 
   public static LayerDefinition createLayer()
   {
-    CubeDeformation cubeDeformation = CubeDeformation.NONE;
-    MeshDefinition meshdefinition = HumanoidModel.createMesh(cubeDeformation, 0.0F);
+    MeshDefinition meshdefinition = HumanoidModel.createMesh(CubeDeformation.NONE, 0F);
     PartDefinition partdefinition = meshdefinition.getRoot();
 
-    // TODO create hat model
+    // Create hat model
     partdefinition.addOrReplaceChild(
-        "hat",
-        CubeListBuilder.create()
-                       .texOffs(0, 0)
-                       .addBox(
-                           -4.0F, -8.0F, -4.0F,
-                           8.0F, 8.0F, 8.0F,
-                           cubeDeformation.extend(0.5F),
-                           0.25F, 0.25F
-                       ),
-        PartPose.offset(0.0F, 0.0F, 0.0F)
-    );
+            RIM,
+            CubeListBuilder.create()
+                    .texOffs(0, 0)
+                    .addBox(-5.0F, -9.0F, -5.0F,
+                            10.0F, 3.0F, 10.0F),
+            PartPose.offset(0.0F, 0F, 0.0F));
 
     partdefinition.addOrReplaceChild(
-        "left_ear",
-        CubeListBuilder.create()
-                       .texOffs(0, 64 / 4)
-                       .addBox(
-                           -3.0F, -13.0F, -0.5F,
-                           2.0F, 4.0F, 1.0F,
-                           cubeDeformation.extend(0.5F),
-                           0.25F, 0.25F
-                       ),
-        PartPose.offset(0.0F, 0.0F, 0.0F)
-    );
+            LAYER1,
+            CubeListBuilder.create()
+                    .texOffs(33, 64)
+                    .addBox(-4.0F, -11.0F, -6.0F,
+                            8.0F, 4.0F, 8.0F),
+            PartPose.offsetAndRotation(
+                    0.0F, 0F, 0.0F,
+                    -0.2F, 0F, 0F
+            ));
 
     partdefinition.addOrReplaceChild(
-        "right_ear",
-        CubeListBuilder.create()
-                       .texOffs(24/4, 64 / 4)
-                       .addBox(
-                           1.0F, -13.0F, -0.5F,
-                           2.0F, 4.0F, 1.0F,
-                           cubeDeformation.extend(0.5F),
-                           0.25F, 0.25F
-                       ),
-        PartPose.offset(0.0F, 0.0F, 0.0F)
-    );
+            LAYER2,
+            CubeListBuilder.create()
+                    .texOffs(33, 64)
+                    .addBox(-3.0F, -12.0F, -6.5F,
+                            6.0F, 4.0F, 6.0F),
+            PartPose.offsetAndRotation(
+                    0.0F, 0F, 0.0F,
+                    -0.4F, 0F, 0F
+            ));
+
+    partdefinition.addOrReplaceChild(
+            LAYER3,
+            CubeListBuilder.create()
+                    .texOffs(33, 64)
+                    .addBox(-2.0F, -13.0F, -7.0F,
+                            4.0F, 4.0F, 4.0F),
+            PartPose.offsetAndRotation(
+                    0.0F, 0F, 0.0F,
+                    -0.6F, 0F, 0F
+            ));
+
+    partdefinition.addOrReplaceChild(
+            BOBBLE,
+            CubeListBuilder.create()
+                    .texOffs(0, 32)
+                    .addBox(-1.0F, -14.0F, -8.0F,
+                            2.0F, 4.0F, 2.0F),
+            PartPose.offsetAndRotation(
+                    0.0F, 0F, 0.0F,
+                    -0.8F, 0F, 0F
+            ));
 
     return LayerDefinition.create(meshdefinition, 128, 128);
   }
@@ -96,14 +136,12 @@ public class SantamanHatModel<T extends LivingEntity> extends CosmeticArmorModel
   @Override
   protected @NotNull Iterable<ModelPart> headParts()
   {
-    this.hat.copyFrom(this.hat);
-    this.leftEar.copyFrom(this.hat);
-    this.rightEar.copyFrom(this.hat);
-
     return ImmutableList.of(
-        this.hat,
-        this.leftEar,
-        this.rightEar
+            this.rim,
+            this.layer1,
+            this.layer2,
+            this.layer3,
+            this.bobble
     );
   }
 
@@ -123,6 +161,21 @@ public class SantamanHatModel<T extends LivingEntity> extends CosmeticArmorModel
       float headPitch
   ) {
     super.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+    // Make the hat follow the head
+    addHeadPositionAndRotation(this.rim);
+    addHeadPositionAndRotation(this.layer1);
+    addHeadPositionAndRotation(this.layer2);
+    addHeadPositionAndRotation(this.layer3);
+    addHeadPositionAndRotation(this.bobble);
+  }
+
+  public void addHeadPositionAndRotation(ModelPart part) {
+    part.x = partPos.get(part).x() + head.x;
+    part.y = partPos.get(part).y() + head.y;
+    part.z = partPos.get(part).z() + head.z;
+    part.xRot = partRot.get(part).x() + head.xRot;
+    part.yRot = partRot.get(part).y() + head.yRot;
+    part.zRot = partRot.get(part).z() + head.zRot;
   }
 
   @Override
--- src/main/java/com/buncord/kirbyessentials/renderers/SantamanLayer.java
+++ src/main/java/com/buncord/kirbyessentials/renderers/SantamanLayer.java
@@ -45,9 +45,9 @@ public class SantamanLayer<T extends LivingEntity, M extends EntityModel<T>>
   ) {
     poseStack.pushPose();
 
-    // TODO adjust scale and translation for the final model
-    poseStack.scale(10F / 18F,  20F / 30F, 1.0F);
-    poseStack.translate(0.0D, -2.0D, 0.0D);
+    // Adjust scale and translation for the final model
+//    poseStack.scale(10F / 18F,  20F / 30F, 1.0F);
+    poseStack.translate(0.0D, -0.9F, 0.0D);
 
     this.santamanHatModel.setupAnim(entity, animationPosition, animationSpeed, bob, headRot, xRot);
 
