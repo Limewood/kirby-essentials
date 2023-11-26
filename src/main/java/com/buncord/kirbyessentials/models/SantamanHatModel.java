794b72790fc6edd0eca56ba54f33f4c42207c412
Evil Cow <evilsmile@gmail.com>
2023-11-26 13:54:38 +0000

    Placeholder implementation of santa hat for endermen

---
+++ src/main/java/com/buncord/kirbyessentials/models/SantamanHatModel.java
@@ -0,0 +1,134 @@
+package com.buncord.kirbyessentials.models;
+
+import com.buncord.kirbyessentials.KirbyEssentials;
+import com.google.common.collect.ImmutableList;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.NotNull;
+
+@OnlyIn(Dist.CLIENT)
+public class SantamanHatModel<T extends LivingEntity> extends CosmeticArmorModel<T> {
+
+  public static final ModelLayerLocation LAYER_LOCATION =
+      new ModelLayerLocation(
+          new ResourceLocation(KirbyEssentials.MOD_ID, "santaman_hat"),
+          "main"
+      );
+
+  // TODO create hat texture
+  public static final ResourceLocation TEXTURE = new ResourceLocation(
+      KirbyEssentials.MOD_ID,
+      "textures/models/armor/santaman_hat.png"
+  );
+
+  public final ModelPart hat;
+  public final ModelPart leftEar;
+  public final ModelPart rightEar;
+
+  public SantamanHatModel(ModelPart modelPart) {
+    super(modelPart);
+
+    this.hat = modelPart.getChild("hat");
+    this.leftEar = modelPart.getChild("left_ear");
+    this.rightEar = modelPart.getChild("right_ear");
+  }
+
+  public static LayerDefinition createLayer()
+  {
+    CubeDeformation cubeDeformation = CubeDeformation.NONE;
+    MeshDefinition meshdefinition = HumanoidModel.createMesh(cubeDeformation, 0.0F);
+    PartDefinition partdefinition = meshdefinition.getRoot();
+
+    // TODO create hat model
+    partdefinition.addOrReplaceChild(
+        "hat",
+        CubeListBuilder.create()
+                       .texOffs(0, 0)
+                       .addBox(
+                           -4.0F, -8.0F, -4.0F,
+                           8.0F, 8.0F, 8.0F,
+                           cubeDeformation.extend(0.5F),
+                           0.25F, 0.25F
+                       ),
+        PartPose.offset(0.0F, 0.0F, 0.0F)
+    );
+
+    partdefinition.addOrReplaceChild(
+        "left_ear",
+        CubeListBuilder.create()
+                       .texOffs(0, 64 / 4)
+                       .addBox(
+                           -3.0F, -13.0F, -0.5F,
+                           2.0F, 4.0F, 1.0F,
+                           cubeDeformation.extend(0.5F),
+                           0.25F, 0.25F
+                       ),
+        PartPose.offset(0.0F, 0.0F, 0.0F)
+    );
+
+    partdefinition.addOrReplaceChild(
+        "right_ear",
+        CubeListBuilder.create()
+                       .texOffs(24/4, 64 / 4)
+                       .addBox(
+                           1.0F, -13.0F, -0.5F,
+                           2.0F, 4.0F, 1.0F,
+                           cubeDeformation.extend(0.5F),
+                           0.25F, 0.25F
+                       ),
+        PartPose.offset(0.0F, 0.0F, 0.0F)
+    );
+
+    return LayerDefinition.create(meshdefinition, 128, 128);
+  }
+
+  @Override
+  protected @NotNull Iterable<ModelPart> headParts()
+  {
+    this.hat.copyFrom(this.hat);
+    this.leftEar.copyFrom(this.hat);
+    this.rightEar.copyFrom(this.hat);
+
+    return ImmutableList.of(
+        this.hat,
+        this.leftEar,
+        this.rightEar
+    );
+  }
+
+  @Override
+  protected @NotNull Iterable<ModelPart> bodyParts()
+  {
+    return ImmutableList.of();
+  }
+
+  @Override
+  public void setupAnim(
+      @NotNull T entity,
+      float limbSwing,
+      float limbSwingAmount,
+      float ageInTicks,
+      float netHeadYaw,
+      float headPitch
+  ) {
+    super.setupAnim(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+  }
+
+  @Override
+  protected ResourceLocation getTexture()
+  {
+    return TEXTURE;
+  }
+
+}
---
+++ src/main/java/com/buncord/kirbyessentials/renderers/SantamanLayer.java
@@ -0,0 +1,72 @@
+package com.buncord.kirbyessentials.renderers;
+
+import com.buncord.kirbyessentials.models.SantamanHatModel;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.NotNull;
+
+@OnlyIn(Dist.CLIENT)
+public class SantamanLayer<T extends LivingEntity, M extends EntityModel<T>>
+    extends RenderLayer<T, M>
+{
+  private final SantamanHatModel<T> santamanHatModel;
+
+  public SantamanLayer(
+      RenderLayerParent<T, M> renderLayerParent,
+      EntityModelSet entityModelSet
+  ) {
+    super(renderLayerParent);
+    this.santamanHatModel = new SantamanHatModel<>(
+        entityModelSet.bakeLayer(SantamanHatModel.LAYER_LOCATION)
+    );
+  }
+
+  public void render(
+      @NotNull PoseStack poseStack,
+      @NotNull MultiBufferSource multiBufferSource,
+      int packedLightCoords,
+      @NotNull T entity,
+      float animationPosition,
+      float animationSpeed,
+      float partialTick,
+      float bob,
+      float headRot,
+      float xRot
+  ) {
+    poseStack.pushPose();
+
+    // TODO adjust scale and translation for the final model
+    poseStack.scale(10F / 18F,  20F / 30F, 1.0F);
+    poseStack.translate(0.0D, -2.0D, 0.0D);
+
+    this.santamanHatModel.setupAnim(entity, animationPosition, animationSpeed, bob, headRot, xRot);
+
+    VertexConsumer vertexconsumer = multiBufferSource.getBuffer(
+        RenderType.armorCutoutNoCull(SantamanHatModel.TEXTURE)
+    );
+
+    this.santamanHatModel.renderToBuffer(
+        poseStack,
+        vertexconsumer,
+        packedLightCoords,
+        OverlayTexture.NO_OVERLAY,
+        1.0F,
+        1.0F,
+        1.0F,
+        1.0F
+    );
+
+    poseStack.popPose();
+  }
+
+}
